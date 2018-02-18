0ba47e53a65a236cd4878aa29bf44a64c380029b
vivchowdhary <36292069+vivchowdhary@users.noreply.github.com>
2018-02-18 16:47:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,9 +69,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('/article-one', function (req,res){
 res.send(createTemplate(articleOne));
 });
@@ -82,7 +79,9 @@ app.get('/article-three', function (req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
-
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
