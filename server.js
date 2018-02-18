37f4e57507d6b1532bb369137f8eb501fe3e6c70
vivchowdhary <36292069+vivchowdhary@users.noreply.github.com>
2018-02-18 11:21:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/article-one', function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req,res){
-   res.send('Article two is sent here and saved'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
     
 });
 app.get('/article-three', function (req,res){
-   res.send('Article three is sent here and saved'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
 
